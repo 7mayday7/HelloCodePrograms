d027db2c944f756d8cd42dc6ad2663b18d31a115
denis <stroodenis462@mail.ru>
2022-12-15 22:10:37 +0600

    Program Condition If Else added

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "даша")
+{
+    Console.WriteLine("Ура, это ведь Дашка!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
